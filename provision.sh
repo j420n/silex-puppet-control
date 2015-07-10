e253a029acd4f6de7e95e65afca593e6aa568c7a
Jason Brown <jason@silex-consulting.com>
2015-07-10 22:21:15 +0100

    Install puppet instead of puppet-agent.

--- provision.sh
+++ provision.sh
@@ -44,7 +44,7 @@ command -v puppet master >/dev/null 2>&1 || {
 #Test for Puppet Agent.
 command -v puppet >/dev/null 2>&1 || {
                                       echo >&2 "Puppet Agent is required, but it is not installed.  Installing...";
-                                      apt-get -y -f install puppet-agent;
+                                      apt-get -y -f install puppet;
                                      }
 
 #Test for PuppetDB
