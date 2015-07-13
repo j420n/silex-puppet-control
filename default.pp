a451639b352b646f225ed6f589c80799740c0a6a
Jason Brown <jason@silex-consulting.com>
2015-07-13 15:34:09 +0100

    Install xen-linux-system and xen-tools.

--- default.pp
+++ default.pp
@@ -3,4 +3,15 @@ hiera_include('classes')
 class profile::default {
 #This is only used to include "Dynamic configuration" created with hiera.
 notify { "The profile::default class is included by 'default'": withpath => true }
+
+  #Install xen-linux-system.
+  package { "xen-linux-system":
+  ensure => "installed"
+  }
+
+  #Install xen-tools.
+  package { "xen-tools":
+  ensure => "installed"
+  }
+
 }
