eccfbdddaafced4fd0d167c940b842dd60337c27
Jason Brown <jason@silex-consulting.com>
2015-07-15 11:00:04 +0100

    Correct syntax error.

--- xen.pp
+++ xen.pp
@@ -1,7 +1,7 @@
 #This file should be in /etc/puppet/environments/$environment/
 
 notify { "The profile::xen class installs a few packages to get started with a Xen Linux System.": withpath => true }
-notify { "To make full use of the Xen Linux System, You will need to reboot and load the Xen kernel."}
+notify { "To make full use of the Xen Linux System, You will need to reboot and load the Xen kernel.":}
 
 class profile::xen {
   #TODO move these packages into a module.
