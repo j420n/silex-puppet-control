84ebde2626b24e99e69bdb6a5e480337e7ac5bec
Jason Brown <jason@silex-consulting.com>
2015-07-15 10:43:08 +0100

    Update default.pp.

--- default.pp
+++ default.pp
@@ -1,17 +1,6 @@
-#This file should be in /etc/puppet/environments/production/
+#This file should be in /etc/puppet/environments/$environment/
 hiera_include('classes')
 class profile::default {
-#This is only used to include "Dynamic configuration" created with hiera.
-notify { "The profile::default class is included by 'default'": withpath => true }
-
-  #Install xen-linux-system.
-  package { "xen-linux-system":
-  ensure => "installed"
-  }
-
-  #Install xen-tools.
-  package { "xen-tools":
-  ensure => "installed"
-  }
-
+  #This is only used to include "Dynamic configuration" created with hiera.
+  notify { "The profile::default class is included by 'default' to include classes defined by hiera.": withpath => true }
 }
