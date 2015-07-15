5a2f1f7a19d90823cc6c2db6af7c1b3b6ac8fe85
Jason Brown <jason@silex-consulting.com>
2015-07-15 11:39:30 +0100

    Update default profile and add a profile for each type of node.

--- default.pp
+++
@@ -1,6 +0,0 @@
-#This file should be in /etc/puppet/environments/$environment/
-hiera_include('classes')
-class profile::default {
-  #This is only used to include "Dynamic configuration" created with hiera.
-  notify { "The profile::default class is included by 'default' to include classes defined by hiera.": withpath => true }
-}
