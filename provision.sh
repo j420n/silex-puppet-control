2787d07151f2727ff4b3e1949ea1c066092d379d
Jason Brown <jason@silex-consulting.com>
2015-07-31 13:14:50 +0100

    Update provisioning script.

--- provision.sh
+++ provision.sh
@@ -11,33 +11,126 @@ then
   dpkg -i puppetlabs-release-pc1-wheezy.deb
 fi
 
-#INSTALL DOCKER MANUALLY AS IT IS NOT AVAILABLE IN DEBIAN 8 yet. https://lists.debian.org/debian-release/2015/03/msg00685.html
-#Replace incorrect sources url which is created by puppet docker module.
-#sed -i "/deb https:\/\/get.docker.com\/ubuntu docker main*/c\deb http:\/\/http.debian.net\/debian jessie-backports main" /etc/apt/sources.list.d/docker.list
-
-#Install ActiveMQ 5.11.1 manually as 5.6.0 from debian repository is broken.
-#echo >&2 "Installing ActiveMQ...";
-#cd /tmp
-#wget http://mirrors.ukfast.co.uk/sites/ftp.apache.org/activemq/5.11.1/apache-activemq-5.11.1-bin.tar.gz
-#tar -zxvf apache-activemq-5.11.1-bin.tar.gz
-#mv apache-activemq-5.11.1 /usr/local/
-#ln -sf /usr/local/apache-activemq-5.11.1 /usr/local/activemq
-#chown -R activemq: /usr/local/apache-activemq-5.11.1/
-#ln -sf /usr/local/activemq/bin/activemq /usr/bin/activemq
-
-#Set domainname manually as vagrant will only set hostname and uses the domain of the router.
-sed -i '/127.0.1.1 debian8-xen*/d' /etc/hosts
-echo '127.0.1.1 debian8-xen.local.ghost debian8-xen' >> /etc/hosts
-
 apt-get update
-
-#Install docker.io using jessie-backports
-#apt-get -t jessie-backports install "docker.io"
 
 #Install some dependencies
 echo >&2 "Installing git-core and deep_merge.";
 apt-get install git-core hiera-eyaml -y
 gem install deep_merge
+
+INSTALL_MASTER=true;
+INSTALL_AGENT=true;
+INSTALL_ACTIVEMQ=true;
+INSTALL_DOCKER=true;
+HOSTNAME=debian8-xen;
+DOMAINNAME=local.ghost;
+
+install_master() {
+    #Test for Puppet Master
+    #Comment this out if you dont want a puppetmaster service.
+    if [ ! -f /etc/init.d/puppetmaster ];
+    then
+       echo >&2 "Puppetmaster is required, but it is not installed.  Installing...";
+       apt-get -y -f install puppetmaster=3.7.2-4;
+    fi
+
+    #Test for PuppetDB
+    #Comment this out if you dont want a puppetdb service.
+    if [ ! -d /etc/puppetdb ];
+    then
+        echo >&2 "PuppetDB is required, but it is not installed.  Installing...";
+        apt-get -y install puppetdb=2.3.5-1puppetlabs1;
+        echo >&2 "PuppetDB is Installed.";
+    fi
+    #Configure Jetty host and port for puppetdb.
+    sed -i 's/# host = <host>/host = 127.0.1.1/g' /etc/puppetdb/conf.d/jetty.ini
+    sed -i 's/port = 8080/port = 8980/g' /etc/puppetdb/conf.d/jetty.ini
+    sed -i 's/# ssl-host = <host>/ssl-host = 0.0.0.0/g' /etc/puppetdb/conf.d/jetty.ini
+    sed -i 's/# ssl-port = <port>/ssl-port = 8981/g' /etc/puppetdb/conf.d/jetty.ini
+    sed -i 's/ssl-port = 8081/ssl-port = 8981/g' /etc/puppetdb/conf.d/jetty.ini
+    echo >&2 "Do these PuppetDb settings look right?";
+    cat /etc/puppetdb/conf.d/jetty.ini | grep ssl-host
+    cat /etc/puppetdb/conf.d/jetty.ini | grep ssl-port
+}
+
+install_agent() {
+#Test for Puppet Agent.
+    command -v puppet >/dev/null 2>&1 || {
+      echo >&2 "Puppet Agent is required, but it is not installed.  Installing...";
+      apt-get -y -f install puppet=3.7.2-4;
+     }
+    #Test for PuppetDB Terminus
+    #Comment this out if you dont want a puppetdb service.
+    if [ ! -d /etc/puppetdb ];
+    then
+        echo >&2 "PuppetDB Terminus is required, but it is not installed.  Installing...";
+        apt-get -y install puppetdb-terminus=2.3.5-1puppetlabs1;
+        echo >&2 "PuppetDB Terminus is Installed.";
+    fi
+}
+
+install_docker() {
+    #INSTALL DOCKER MANUALLY AS IT IS NOT AVAILABLE IN DEBIAN 8 yet. https://lists.debian.org/debian-release/2015/03/msg00685.html
+    if [ -f /etc/apt/sources.list.d/docker.list ]
+    then
+      rm /etc/apt/sources.list.d/docker.list;
+    fi
+    touch /etc/apt/sources.list.d/docker.list
+    echo "deb http://http.debian.net/debian jessie-backports main" >> /etc/apt/sources.list.d/docker.list
+    #Replace incorrect sources url which is created by puppet docker module.
+    #sed -i "/deb https:\/\/get.docker.com\/ubuntu docker main*/c\deb http:\/\/http.debian.net\/debian jessie-backports main" /etc/apt/sources.list.d/docker.list
+    #Install docker.io using jessie-backports
+    echo >&2 "Installing Docker.io...";
+    apt-get update
+    apt-get -t jessie-backports install "docker.io" -y
+    ln -sf /usr/bin/docker.io /usr/bin/docker
+}
+
+install_activemq() {
+    #Install ActiveMQ 5.11.1 manually as 5.6.0 from debian repository is broken.
+    command -v activemq >/dev/null 2>&1 || {
+        echo >&2 "Installing ActiveMQ...";
+        cd /tmp
+        if [ ! -f apache-activemq-5.11.1-bin.tar.gz ]
+          then
+            wget http://mirrors.ukfast.co.uk/sites/ftp.apache.org/activemq/5.11.1/apache-activemq-5.11.1-bin.tar.gz
+            gunzip -c apache-activemq-5.11.1-bin.tar.gz | tar zxvf -
+        fi
+        mv apache-activemq-5.11.1 /usr/local/
+        ln -sf /usr/local/apache-activemq-5.11.1 /usr/local/activemq
+        chown -R activemq: /usr/local/apache-activemq-5.11.1/
+        ln -sf /usr/local/activemq/bin/activemq /usr/bin/activemq
+    }
+
+}
+
+#Set domainname manually as vagrant will only set hostname and uses the domain of the router.
+sed -i '/127.0.1.1 */d' /etc/hosts
+echo "127.0.1.1 $HOSTNAME.$DOMAINNAME $HOSTNAME" >> /etc/hosts
+
+if [ ${INSTALL_AGENT} ];
+then
+install_agent
+fi
+
+if [ ${INSTALL_MASTER} ];
+then
+install_master
+fi
+
+if [ ${INSTALL_DOCKER} ];
+then
+install_docker
+fi
+
+if [ ${INSTALL_ACTIVEMQ} ];
+then
+install_activemq
+fi
+
+
+#Replace certname in puppet.conf.
+sed -i "/certname = */c\certname = $(facter fqdn)" /etc/puppet/puppet.conf
 
 #Pull any updates.
 if [ -d /etc/puppet/silex-puppet-control ];
@@ -47,14 +140,8 @@ then
     git pull origin vagrant
 fi
 
-#Test for Puppet Agent.
-command -v puppet >/dev/null 2>&1 || {
-                                      echo >&2 "Puppet Agent is required, but it is not installed.  Installing...";
-                                      apt-get -y -f install puppet=3.7.2-4;
-                                     }
-
 #Check if we have already cloned the repo.
-if [ ! -d /etc/puppet/silex-puppet-control ];
+if [ ! -d /etc/puppet/silex-puppet-control ] && [ -d /etc/puppet ];
 then
     echo >&2 "Cloning the 'Control Repo'";
     cd /etc/puppet
@@ -64,29 +151,10 @@ then
     git checkout vagrant
 fi
 
-#Test for Puppet Master
-#Comment this out if you dont want a puppetmaster service.
-if [ ! -f /etc/init.d/puppetmaster ];
-then
-   echo >&2 "Puppetmaster is required, but it is not installed.  Installing...";
-   apt-get -y -f install puppetmaster=3.7.2-4;
-fi
-
-#Test for PuppetDB
-#Comment this out if you dont want a puppetdb service.
-if [ ! -d /etc/puppetdb ];
-then
-    echo >&2 "PuppetDB is required, but it is not installed.  Installing...";
-    apt-get -y install puppetdb=2.3.5-1puppetlabs1;
-    apt-get -y install puppetdb-terminus=2.3.5-1puppetlabs1;
-    echo >&2 "PuppetDB is Installed.";
-fi
-
-#Create Keys
-echo >&2 "Creating Keys";
-
 if [ ! -f /etc/puppet/silex-puppet-control/keys/private_key.pkcs7.pem ];
 then
+    #Create Keys
+    echo >&2 "Creating Keys";
     cd /etc/puppet/silex-puppet-control/
     eyaml createkeys
 fi
@@ -110,35 +178,26 @@ ln -sf /etc/puppet/silex-puppet-control/environment.conf /etc/puppet/
 
 echo >&2 "Deploying 'VAGRANT' environment...";
 r10k deploy environment vagrant
-
-#Configure Jetty host and port for puppetdb.
-sed -i 's/# host = <host>/host = 127.0.1.1/g' /etc/puppetdb/conf.d/jetty.ini
-sed -i 's/port = 8080/port = 8980/g' /etc/puppetdb/conf.d/jetty.ini
-sed -i 's/# ssl-host = <host>/ssl-host = 0.0.0.0/g' /etc/puppetdb/conf.d/jetty.ini
-sed -i 's/# ssl-port = <port>/ssl-port = 8981/g' /etc/puppetdb/conf.d/jetty.ini
-sed -i 's/ssl-port = 8081/ssl-port = 8981/g' /etc/puppetdb/conf.d/jetty.ini
-echo >&2 "Do these PuppetDb settings look right?";
-cat /etc/puppetdb/conf.d/jetty.ini | grep ssl-host
-cat /etc/puppetdb/conf.d/jetty.ini | grep ssl-port
-
-#Replace certname in puppet.conf.
-sed -i "/certname = */c\certname = $(facter fqdn)" /etc/puppet/puppet.conf
+echo >&2 "Restarting Services";
 
 if [ -f /etc/init.d/puppetmaster ];
 then
     echo >&2 "The local Puppet Master has been found. Welcome to the Puppet show.";
-    /etc/init.d/puppetdb restart;
     /etc/init.d/puppetmaster restart;
-    /etc/init.d/puppetqd restart;
-    puppet agent --enable
-    puppetdb ssl-setup
 fi
 
-echo >&2 "Waiting 30 seconds for PuppetDB to start.";
-until netstat -antpu | grep 8981;
-  do sleep 30;
-done
+if [ -f /etc/init.d/puppetdb ];
+then
+    puppetdb ssl-setup
+    /etc/init.d/puppetdb restart;
+    /etc/init.d/puppetqd restart;
+    echo >&2 "Waiting 30 seconds for PuppetDB to start.";
+    until netstat -antpu | grep 8981;
+      do sleep 30;
+    done
+fi
 
 echo >&2 "Running Puppet Agent.";
+puppet agent --enable
 puppet agent -t
 
